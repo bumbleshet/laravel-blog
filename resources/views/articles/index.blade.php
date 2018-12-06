8b4986e1c4b18ae3c6def6352df1bb044a9f1d52
Sidney F. Guaro <36142076+bumbleshet@users.noreply.github.com>
2018-12-06 18:54:36 +0800

    Update index.blade.php

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 
-<h2>Articles</h2>
+<h2>Blogs</h2>
 <hr>
  @foreach($articles as $article)
  	<article>
-		<h3><a href="{{ url('articles/show/'.$article->id) }}"
+		<h3><a href="{{ url('blogs/show/'.$article->id) }}"
 			..
 			>{{$article->title}}</a></h3>
  		<p>{{$article->body}}</p>
- 		<a href="{{ url('articles/tags/'.$category[$article->category]) }}">{{$category[$article->category]}}</a>
+ 		<a href="{{ url('blogs/tags/'.$category[$article->category]) }}">{{$category[$article->category]}}</a>
  	</article>
 @endforeach
 
